9619b4975d81990fcfbfe2b320c254239f068e3b
Martin Sit <46298951+martin226@users.noreply.github.com>
2024-05-11 14:47:39 -0400

    feat: about and help panels

--- AboutPanel.java
+++ AboutPanel.java
@@ -3,26 +3,39 @@ import javax.swing.JLabel;
 import javax.swing.JPanel;
 
 class AboutPanel extends JPanel {
-    JLabel programmerLabel = new JLabel();
-    JLabel versionLabel = new JLabel();
-    JLabel courseLabel = new JLabel();
+    JLabel programmerLabel = new JLabel("Developed by Kaden Seto and Martin Sit");
+    JLabel versionLabel = new JLabel("Version 1.0");
+    JLabel courseLabel = new JLabel("St. Augustine CHS Computer Science - ICS4U1");
+    JLabel teacherLabel = new JLabel("Taught by Mr. Cadawas");
 
     // Constructor
     AboutPanel() {
         super();
-
+        // Set the layout to null
         setLayout(null);
 
-        programmerLabel.setText("Developed by Kaden Seto and Martin Sit");
-        programmerLabel.setBounds(10, 10, 300, 20);
+        // Set the size and location of the programmerLabel
+        programmerLabel.setSize(400, 40);
+        programmerLabel.setFont(programmerLabel.getFont().deriveFont(16.0f));
+        programmerLabel.setLocation(10, 10);
         add(programmerLabel);
 
-        versionLabel.setText("Version 1.0");
-        versionLabel.setBounds(10, 30, 300, 20);
+        // Set the size and location of the versionLabel
+        versionLabel.setSize(400, 40);
+        versionLabel.setFont(versionLabel.getFont().deriveFont(16.0f));
+        versionLabel.setLocation(10, 50);
         add(versionLabel);
 
-        courseLabel.setText("St. Augustine CHS Computer Science - ICS4U1 - Mr. Cadawas");
-        courseLabel.setBounds(10, 50, 500, 20);
+        // Set the size and location of the courseLabel
+        courseLabel.setSize(400, 40);
+        courseLabel.setFont(courseLabel.getFont().deriveFont(16.0f));
+        courseLabel.setLocation(10, 90);
         add(courseLabel);
+
+        // Set the size and location of the teacherLabel
+        teacherLabel.setSize(400, 40);
+        teacherLabel.setFont(teacherLabel.getFont().deriveFont(16.0f));
+        teacherLabel.setLocation(10, 130);
+        add(teacherLabel);
     }
 }
--- HelpPanel.java
+++ HelpPanel.java
@@ -1,8 +1,45 @@
+import javax.imageio.ImageIO;
 import javax.swing.JPanel;
 
+import java.awt.Graphics;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+
 class HelpPanel extends JPanel {
+    BufferedImage imgHelp;
+
+    @Override
+    public void paintComponent(Graphics g) {
+        super.paintComponent(g);
+        g.drawImage(imgHelp, 105, 60, 750, 420, null);
+    }
+
     // Constructor
     HelpPanel() {
         super();
+
+        // Set the layout to null
+        setLayout(null);
+
+        //Try to read the image from both the jar file and local drive
+        InputStream helpClass = this.getClass().getResourceAsStream("HelpPanel.png");
+
+        if (helpClass != null) {
+            try {
+                imgHelp = ImageIO.read(helpClass);
+            } catch (IOException e) {
+                System.out.println("Unable to read/load image from jar");
+                e.printStackTrace();
+            }
+        } else { //If it can't be found on the jar, search it locally
+            try {
+                imgHelp = ImageIO.read(new File("HelpPanel.png"));
+            } catch (IOException e) {
+                System.out.println("Unable to read/load image");
+                e.printStackTrace();
+            }
+        }
     }
 }
